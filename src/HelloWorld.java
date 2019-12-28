77847ad70329b4501cdd59cf946e5286ef22b9a1
Immortality Dark <zhenya.bronza@gmail.com>
2019-12-29 00:28:45 +0200

    Method InPut overloading
    AUDIOPLAY-28 Add new overloading method (double)

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,13 +1,16 @@
-
 public class HelloWorld {
 	public static void main(String[] args) {
-		  System.out.println("Hello World!");
-		  InPut("WOW");
+		System.out.println("Hello World!");
+		InPut("WOW");
+		InPut(3.14);
 	} 
 	
-
-public static void InPut(String h){
-	System.out.println(h);
-}
+	public static void InPut(String h){
+		System.out.println(h);
+	}
+	
+	public static void InPut(double h){
+		System.out.println(h);
+	}
 
 }
