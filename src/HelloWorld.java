6b89bd4389b8a19106f73ffcd147afe1e05dfb38
Sheblyk <kokosigel@gmail.com>
2019-12-29 00:15:01 +0200

    WowInHW
    AUDIOPLAY-28 #comment add new function

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,19 +1,13 @@
-/**
- * 
- */
 
-/**
- * @author Acer
- *
- */
 public class HelloWorld {
-
-	/**
-	 * @param args
-	 */
 	public static void main(String[] args) {
-		  System.out.println("Hello World!");		
+		  System.out.println("Hello World!");
+		  InPut("WOW");
+	} 
+	
 
-	}
+public static void InPut(String h){
+	System.out.println(h);
+}
 
 }
