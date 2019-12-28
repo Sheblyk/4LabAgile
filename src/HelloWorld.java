a998a4b290b3491d45e84c7cf0cd5dbec1b7d067
Sheblyk <kokosigel@gmail.com>
2019-12-28 23:46:39 +0200

    HW
    Add Class Hello World

---
+++ src/HelloWorld.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+
+/**
+ * @author Acer
+ *
+ */
+public class HelloWorld {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		  System.out.println("Hello World!");		
+
+	}
+
+}
